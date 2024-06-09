ad06beee6f347ff9fa258de42c217f9c9bdcad76
Phant0m The Great <120599821+Phant0mthegreat@users.noreply.github.com>
2024-06-09 18:08:09 -0300

    Update pc.py

--- pc.py
+++ pc.py
@@ -4,42 +4,71 @@ import banners
 from pystyle import Colorate, Colors
 from rich.panel import Panel
 from rich.console import Console
+from InquirerPy import prompt
+from InquirerPy.base.control import Choice
+from rich.style import Style
 import sys
 import util
+
 console = Console()
 
+question_style = Style(color="red", bold=True)
+choice_style = Style(color="red")
+pointer_style = Style(color="red")
+selected_style = Style(color="red", bold=True)
+
+questions = [
+    {
+        "type": "list",
+        "name": "choice",
+        "message": ("                                               What do you want?"),
+        "choices": [
+            ("                                               1. Hide message in an image"),
+            ("                                               2. Scan image"),
+            ("                                               3. Information"),
+            ("                                               4. Exit"),
+        ],
+    }
+]
+
 try:
-  util.loading_pc()
-  while True:
-    os.system('clear')
-    print(Colorate.Vertical(Colors.yellow_to_red, banners.banner1))
-    console.print(Panel.fit("""{[yellow]1[white]} Hide message in an image\n\n{[yellow]2[white]} Scan image\n\n{[yellow]3[white]} Information\n\n{[yellow]4[white]} Exit""",padding=(0, 40), title="🔎"))
-    respost = input(f'Enter your choice {c.byellow}>{c.white}')
-    if respost == '1':
-      os.system('clear')
-      print(Colorate.Vertical(Colors.yellow_to_red, banners.banner1))
-      image_path = input(f"{c.bwhite}[{c.byellow}>{c.bwhite}]{c.white} Enter the path to the image: ")
-      message_to_hide = input(f"{c.bwhite}[{c.byellow}>{c.bwhite}]{c.white} Enter the message to be hidden in the image: ")
-      if util.hide_message(image_path, message_to_hide):
-        print(f"\n{c.green}Message successfully hidden in {image_path} !{c.white}")
-        input(f"\n{c.white}Press {c.bwhite}[ENTER]{c.white} to continue")
-    elif respost == '2':
-      os.system('clear')
-      print(Colorate.Vertical(Colors.yellow_to_red, banners.banner1))
-      image_path = input(f"{c.bwhite}[{c.byellow}>{c.bwhite}]{c.white} Enter the path of the image with the hidden message: ")
-      message_recovered = util.scan_image_metadata(image_path)
-      if message_recovered:
-       print(f'\n{message_recovered}')
-       input(f"\n{c.white}Press {c.bwhite}[ENTER]{c.white} to continue")
-    elif respost == '3':
-      os.system('clear')
-      print(Colorate.Vertical(Colors.yellow_to_red, banners.banner2))
-      print(f'''{c.bwhite}[{c.byellow}About 📖{c.bwhite}] {c.white}Stark is a tool focused on steganography and image analysis, with varied purposes.\n\n{c.bwhite}[{c.bcyan}Creator Discord 👔{c.bwhite}] {c.white}@phant0mthegreat''')
-      input(f"\n{c.white}Press {c.bwhite}[ENTER]{c.white} to continue")
-    elif respost == '4':
-      os.system('clear')
-      print(Colorate.Vertical(Colors.yellow_to_red, banners.banner3))
-      print(f'{c.byellow}Thank you for using Stark')
-      sys.exit()
+    util.loading_pc()
+    while True:
+        os.system('clear')
+        print(Colorate.Vertical(Colors.yellow_to_red, banners.banner1))
+        answers = prompt(questions, style={
+            "question": "fg:ansired bold",
+            "pointer": "fg:ansired",
+            "highlight": "fg:ansired bold",
+            "answer": "fg:ansired bold",
+        })
+        respost = answers['choice']
+
+        if "1" in respost:
+            os.system('clear')
+            print(Colorate.Vertical(Colors.yellow_to_red, banners.banner1))
+            image_path = input(f"{c.bwhite}[{c.byellow}>{c.bwhite}]{c.white} Enter the path to the image: ")
+            message_to_hide = input(f"{c.bwhite}[{c.byellow}>{c.bwhite}]{c.white} Enter the message to be hidden in the image: ")
+            if util.hide_message(image_path, message_to_hide):
+                print(f"\n{c.green}Message successfully hidden in {image_path} !{c.white}")
+                input(f"\n{c.white}Press {c.bwhite}[ENTER]{c.white} to continue")
+        elif "2" in respost:
+            os.system('clear')
+            print(Colorate.Vertical(Colors.yellow_to_red, banners.banner1))
+            image_path = input(f"{c.bwhite}[{c.byellow}>{c.bwhite}]{c.white} Enter the path of the image with the hidden message: ")
+            message_recovered = util.scan_image_metadata(image_path)
+            if message_recovered:
+                print(f'\n{message_recovered}')
+                input(f"\n{c.white}Press {c.bwhite}[ENTER]{c.white} to continue")
+        elif "3" in respost:
+            os.system('clear')
+            print(Colorate.Vertical(Colors.yellow_to_red, banners.banner2))
+            print(f'''{c.bwhite}[{c.byellow}About 📖{c.bwhite}] {c.white}Stark is a tool focused on steganography and image analysis, with varied purposes.\n\n{c.bwhite}[{c.bcyan}Creator Discord 👔{c.bwhite}] {c.white}@phant0mthegreat''')
+            input(f"\n{c.white}Press {c.bwhite}[ENTER]{c.white} to continue")
+        elif "4" in respost:
+            os.system('clear')
+            print(Colorate.Vertical(Colors.yellow_to_red, banners.banner3))
+            print(f'{c.byellow}Thank you for using Stark')
+            sys.exit()
 except KeyboardInterrupt:
-  print('\n[#] Program interrupted')
+    print('\n[#] Program interrupted')
