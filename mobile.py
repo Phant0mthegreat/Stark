3c3cc34e85a328710c6341748366a11969aa4743
Phant0m The Great <120599821+Phant0mthegreat@users.noreply.github.com>
2024-06-12 18:29:59 -0300

    Update mobile.py

--- mobile.py
+++ mobile.py
@@ -25,7 +25,7 @@ questions = [
         "choices": [
             ("1. Hide message in an image"),
             ("2. Scan image"),
-            ("3. Information"),
+            ("3. Software Informations"),
             ("4. Exit"),
         ],
     }
@@ -37,6 +37,7 @@ try:
   while True:
     os.system('clear')
     print(Colorate.Vertical(Colors.yellow_to_red, banners.banner4))
+    print(f"                  {c.lwhite}v{util.VERSION}")
     answers = prompt(questions, style={
             "question": "fg:ansired bold",
             "pointer": "fg:ansired",
@@ -48,7 +49,7 @@ try:
     if "1" in respost:
       os.system('clear')
       print(Colorate.Vertical(Colors.yellow_to_red, banners.banner4))
-      image_path = input(f"{c.bwhite}[{c.byellow}>{c.bwhite}]{c.white} Enter the path to the image: ")
+      image_path = input(f"\n{c.bwhite}[{c.byellow}>{c.bwhite}]{c.white} Enter the path to the image: ")
       message_to_hide = input(f"{c.bwhite}[{c.byellow}>{c.bwhite}]{c.white} Enter the message to be hidden in the image: ")
       if util.hide_message(image_path, message_to_hide):
         print(f"\n{c.green}Message successfully hidden in {image_path} !{c.white}")
@@ -56,16 +57,27 @@ try:
     elif "2" in respost:
       os.system('clear')
       print(Colorate.Vertical(Colors.yellow_to_red, banners.banner4))
-      image_path = input(f"{c.bwhite}[{c.byellow}>{c.bwhite}]{c.white} Enter the path of the image with the hidden message: ")
+      image_path = input(f"\n{c.bwhite}[{c.byellow}>{c.bwhite}]{c.white} Enter the path of the image with the hidden message: ")
       message_recovered = util.scan_image_metadata(image_path)
       if message_recovered:
        print(f'\n{message_recovered}')
        input(f"\n{c.white}Press {c.bwhite}[ENTER]{c.white} to continue")
     elif "3" in respost:
-      os.system('clear')
-      print(Colorate.Vertical(Colors.yellow_to_red, banners.banner2))
-      print(f'''{c.bwhite}[{c.byellow}About 📖{c.bwhite}] {c.white}Stark is a tool focused on steganography and image analysis, with varied purposes.\n\n{c.bwhite}[{c.bcyan}Creator Discord 👔{c.bwhite}] {c.white}@phant0mthegreat''')
-      input(f"\n{c.white}Press {c.bwhite}[ENTER]{c.white} to continue")
+            os.system('clear')
+            print(Colorate.Vertical(Colors.yellow_to_red, banners.banner2))
+            print(banners.options)
+            respost_informations = input(f"{c.bwhite}[{c.byellow}>{c.bwhite}]{c.white} You choice: ")
+            match respost_informations:
+                case "1":
+                    os.system('clear')
+                    print(Colorate.Vertical(Colors.red_to_yellow, banners.banner6))
+                    print(f"\n{c.byellow}[{c.white}Creator 👔 {c.byellow}]{c.white}Phant0m The Great{c.white}\n\n{c.byellow}[{c.white}Discord Contact 🪪 {c.byellow}]{c.white}phant0mthegreat")
+                    input(f"\nPress {c.bwhite}[ENTER]{c.white} to continue")
+                case "2":
+                    os.system('clear')
+                    print(Colorate.Vertical(Colors.red_to_yellow, banners.banner7))
+                    print(f"\n{c.byellow}[{c.white}Version ⚙️ {c.byellow}]{c.white}{util.VERSION}\n\n{c.byellow}[{c.white}Programming L. 💾 {c.byellow}]{c.white}Python\n\n{c.byellow}[{c.white}Total lines of code 🩻 {c.byellow}]{c.white}12122")
+                    input(f"\nPress {c.bwhite}[ENTER]{c.white} to continue")
     elif "4" in respost:
       os.system('clear')
       print(Colorate.Vertical(Colors.yellow_to_red, banners.banner3))
