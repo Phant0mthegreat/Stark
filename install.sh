eb6607886f0256badd6de03ecf2ee7acaeae80bb
Phant0m The Great <120599821+Phant0mthegreat@users.noreply.github.com>
2024-08-09 15:54:15 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -14,6 +14,7 @@ m1="$b\033[35m"
 a="$b\033[1;33m"
 a1="$b\033[33m"
 cy="$b\033[38;2;23;147;209m"
+
 clear
 echo -e "${r}
           ..--
@@ -42,9 +43,34 @@ MM##++        ########@@
                                         ####    ##
                                           ########
 ${b}"
-echo -e "[${r1}+${b}] Installing packages...\n"
-if pip install rich && pip install pystyle && pip install pillow && pip install piexif && pip install InquirerPy; then
-  echo -e "\n[${v1}+${b}] Good news, the packages were installed successfully"
+echo -e "[${r1}+${b}] Detecting environment...\n"
+
+OS=$(uname -o)
+if [ "$OS" == "Android" ]; then
+  echo -e "[${v1}+${b}] Termux environment detected.\n"
+  
+  if apt update && apt install -y libjpeg-turbo pcre libpng zlib; then
+    echo -e "\n[${v1}+${b}] Dependencies installed successfully in Termux."
+  else
+    echo -e "\n[${r1}+${b}] Failed to install dependencies in Termux. Please try manually."
+    exit 1
+  fi
+elif [ "$OS" == "GNU/Linux" ]; then
+  echo -e "[${v1}+${b}] Kali Linux environment detected.\n"
+  
+  if sudo apt update && sudo apt install -y libjpeg-dev libpng-dev zlib1g-dev; then
+    echo -e "\n[${v1}+${b}] Dependencies installed successfully in Kali Linux."
+  else
+    echo -e "\n[${r1}+${b}] Failed to install dependencies in Kali Linux. Please try manually."
+    exit 1
+  fi
+else
+  echo -e "[${r1}+${b}] Unknown environment. Exiting..."
+  exit 1
+fi
+
+if pip install rich pystyle pillow piexif InquirerPy; then
+  echo -e "\n[${v1}+${b}] Good news, the packages were installed successfully."
   sleep 2
   echo -e "\nStark will start automatically in a few seconds, if you want to start it again, just go to the repository and type ${r}python3 Stark.py${b}"
   sleep 3
@@ -63,5 +89,5 @@ if pip install rich && pip install pystyle && pip install pillow && pip install 
   clear
   python3 Stark.py
 else
-  echo "[${r}+${b}] Bad news, we were unable to install the packages on your machine, please try again later."
+  echo "[${r1}+${b}] Bad news, we were unable to install the packages on your machine, please try again later."
 fi
